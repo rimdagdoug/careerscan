301c6140bc1ea8f8e2ecdaccb29d7918dd37ae38
Rim Dagdoug <rimdagdoug99@gmail.com>
2026-09-23 17:51:20 +0200

    second commit

---
+++ src/main/java/com/careerscan/careerscan/CareerscanApplication.java
@@ -0,0 +1,13 @@
+package com.careerscan.careerscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerscanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerscan/careerscan/CareerscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerscan.careerscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
